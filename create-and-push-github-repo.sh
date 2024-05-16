5eb3855479271245d22d462dd4eb7aa996633769
Ibrahim <Ibrahim@ebs.net.pk>
2024-05-16 23:18:47 +0200

    update code

--- create-and-push-github-repo.sh
+++ create-and-push-github-repo.sh
@@ -3,10 +3,8 @@
 current_dir=$(basename $(pwd))
 
 echo "This script will create a GitHub repository named '$current_dir'."
-echo "The remote named 'origin' will be added (if it doesn't exist) and your code will be pushed."
+echo "The remote named 'origin' will be added  and your code will be pushed."
 read -p "Are you sure you want to continue? (y/n) " confirmation
-
-# Check if the repository exists (optional)
 
 
 if [[ "$confirmation" =~ ^[Yy]$ ]]; then
@@ -27,15 +25,7 @@ if [[ "$confirmation" =~ ^[Yy]$ ]]; then
 
   # Create the repository on GitHub and Specify remote name directly
   gh_repo_create_output=$(gh repo create "$current_dir" --public)
-  postfix= ".git"
-  echo "---------------------------------"
-  echo "$gh_repo_create_output"
-  echo "---------------------------------"
-  remote_url="$gh_repo_create_output$postfix"
-  
-  echo "---------------------------------"
-  echo "Remote URL: $remote_url"
-  echo "---------------------------------"
+  remote_url="$gh_repo_create_output"
 
   # Check if the remote URL was retrieved successfully
   if [[ -z "$remote_url" ]]; then
@@ -43,11 +33,9 @@ if [[ "$confirmation" =~ ^[Yy]$ ]]; then
     exit 1
   fi
 
-  # Add the remote named "origin" (if it doesn't exist)
   git remote add origin "$remote_url.git"
-
-  # Push your code to the remote repository (origin main)
   git push
+
 else
   echo "Aborting..."
   read -p "Would you like to create a repository with a custom name? (y/n) " custom_name_prompt
@@ -59,7 +47,18 @@ else
     # Create the repository with the custom name
     gh repo create "$custom_name" &> /dev/null
 
-    # ... (remaining script logic for adding remote and pushing using the custom_name)
+    # Create the repository on GitHub and Specify remote name directly
+    gh_repo_create_output=$(gh repo create "$custom_name" --public)
+    remote_url="$gh_repo_create_output"
+
+    # Check if the remote URL was retrieved successfully
+    if [[ -z "$remote_url" ]]; then
+      echo "Failed to retrieve remote URL for '$current_dir'."
+      exit 1
+    fi
+
+    git remote add origin "$remote_url.git"
+    git push
 
   else
     echo "No action taken."
